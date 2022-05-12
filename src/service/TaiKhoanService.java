218da70a137a508b7e7b3cf6b030e7e5a9b38264
dat2072002 <101373340+dat2072002@users.noreply.github.com>
2022-05-12 23:35:54 +0700

    Update TaiKhoanService.java

--- src/service/TaiKhoanService.java
+++ src/service/TaiKhoanService.java
@@ -6,6 +6,6 @@ import model.TaiKhoan;
 
 public interface TaiKhoanService {
     
-    public TaiKhoan login(String tdn, String mk);
+    public TaiKhoan login(String tdn, String mk); // Dang Nhap Tai Khoan bang ten dang nhap va mat khau
     
 }
