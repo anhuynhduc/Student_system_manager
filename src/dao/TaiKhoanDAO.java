ac82449dbdd895f577154a5f225d1b92d98ef4a9
Lê Văn Hiếu <104577452+levanhieu652002@users.noreply.github.com>
2022-05-13 23:01:22 +0700

    Update TaiKhoanDAO.java

--- src/dao/TaiKhoanDAO.java
+++ src/dao/TaiKhoanDAO.java
@@ -8,5 +8,5 @@ import model.TaiKhoan;
  * @author Admin
  */
 public interface TaiKhoanDAO {
-    public TaiKhoan login(String tdn, String mk);
+    public TaiKhoan login(String tdn, String mk);// tạo tài khoản và mật khẩu
 }
